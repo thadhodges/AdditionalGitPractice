c0b2a5b0ac83004db4fc7253ea7533ea075769cc
Thad <thadisfree@gmail.com>
2021-06-28 21:03:25 -0500

    added some more lines

--- AdditionalGitPractice/Program.cs
+++ AdditionalGitPractice/Program.cs
@@ -8,7 +8,11 @@ namespace AdditionalGitPractice
         {
             Console.WriteLine("What is your name, Please?");
             var name = Console.ReadLine();
-            Console.WriteLine($"Hello, {name}, Nice to meet you!");
+            Console.WriteLine($"Hello, {name}, Nice to meet you!/n How are you doing today?");
+            var doing = Console.ReadLine();
+            Console.WriteLine($"It's good to know you are doing {doing}.../n" +
+                $"Mental Health is very important, Don't you agree?");
+            Console.ReadLine();
         }
     }
 }
